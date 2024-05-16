41120d32e1dca8fefb84b20002538ee107c1a1b0
gardentwo <nikita.karpicheff@yandex.ru>
2024-05-16 22:20:51 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#include<8051.h>
+
+int flag = 0;
+
+interrupt void isr_ex0(void)
+{
+	if (flag == 0) flag = 1;
+ 	else flag = 0;
+}
+
+
+void main()
+{
+	P32=0;
+	IT0 = 1;
+	EX0 = 1;
+	EA = 1;
+	P1 = 0xff;
+	P2 = 0x00;
+	IE=0x81; 
+	while(1){ 
+		P36 = 1;
+		P36 = 0;
+		while(P37 == 1);
+		if (flag == 1) P2 = P1;
+		else P2 = P1+P1/5;	
+	}
+}
